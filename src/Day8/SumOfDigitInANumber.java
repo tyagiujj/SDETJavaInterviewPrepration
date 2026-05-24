a586ff1760096a3b47da235fdc4994c323a051bd
Ujjwal Tyagi <ujjwaltyagi9700@gmail.com>
2026-05-24 10:35:42 +0530

    Added the Sum of Digit in a Number

---
+++ src/Day8/SumOfDigitInANumber.java
@@ -0,0 +1,20 @@
+package Day8;
+
+import java.util.Scanner;
+
+public class SumOfDigitInANumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int num=sc.nextInt();
+		int sum=0;
+		while(num!=0) {
+		sum=sum+num%10;
+		num =num/10;
+		}
+		System.out.println("Sum of the Digit in a Number is : " + sum);
+
+	}
+
+}
