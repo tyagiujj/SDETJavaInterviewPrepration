24cd5d98df0a2a670eb814a30edfd233b61e2125
Ujjwal Tyagi <ujjwaltyagi9700@gmail.com>
2026-05-30 12:14:24 +0530

    Added the programe of GenerateRandomNumber

---
+++ src/Day12/GenerateNumbersAndStrings.java
@@ -0,0 +1,25 @@
+package Day12;
+
+import java.util.Random;
+
+public class GenerateNumbersAndStrings {
+
+	public static void main(String[] args) {
+ // Approach 1-Random class
+//		Random randm=new Random();
+//		int rand_int=randm.nextInt(1000);
+//		System.out.println(rand_int);
+//		
+//		double rand_bdl=randm.nextDouble();  //range 0.0  and less than 1.0
+//		System.out.println(rand_bdl);
+		
+		
+//Approach 2 Math class
+		System.out.println(Math.random());
+		
+		
+
+
+	}
+
+}
